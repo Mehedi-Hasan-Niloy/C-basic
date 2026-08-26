c0d7440535036467a384850a705d6f07613bd494
Mehedi Hasan <130240814+Mehedi-Hasan-Niloy@users.noreply.github.com>
2026-08-26 10:46:17 +0600

    Add C program to demonstrate float, double, and char data types
    This program shows how to declare float, double, and char variables and print their values using their respective format specifiers (%f, %lf, %c) in C.

---
+++ float_double_char.c
@@ -0,0 +1,16 @@
+/*
+  write a program that prints a floating,
+  double number and character
+*/
+#include<stdio.h>
+int main()  
+{
+  float num1=10.5;
+  double num2=10.5255;
+  char ch='a';
+  printf("num1=%.1f\n",num1);//Jodi num1 e point er por 1 ghor nite chai tahole %.1f likhte hobe
+  printf("num2=%lf\n",num2);
+  printf("ch=%c\n",ch);
+  return 0;
+
+}
