9fdcc5f19c5392ce22f789c005f585b0f09e437e
Mehedi Hasan <130240814+Mehedi-Hasan-Niloy@users.noreply.github.com>
2026-08-26 10:15:24 +0600

    Add basic C program to print name and number
    This C program demonstrates the use of basic printf function, newline character (\n), and how to write single & multiple-line comments.

---
+++ c-2.c
@@ -0,0 +1,11 @@
+/*
+   multiple line comment 
+   jekono line e use kora Jabe
+*/
+#include<stdio.h>
+int main()
+{
+   printf("Mehedi Hasan\n");//new line er jonno \n
+   printf("018304948");
+   getchar();
+}
