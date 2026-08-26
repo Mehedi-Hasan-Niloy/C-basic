c192a7922a89dfb30534aa15d8994c95b95f8bd4
Mehedi Hasan <130240814+Mehedi-Hasan-Niloy@users.noreply.github.com>
2026-08-26 10:29:38 +0600

    Create print_integer.c
    This program demonstrates how to declare an integer variable and print its value using the %d format specifier in C.

---
+++ c-3.c
@@ -0,0 +1,12 @@
+/* 
+ An integar variable num that will contain 10
+ write a program that prints an integer number
+*/
+#include<stdio.h> 
+int main()
+{
+  int num=10;
+  printf("Number is %d",num);
+
+
+}
