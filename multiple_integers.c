4fed41ccf94b1fb7dd2b9cb07b126480b7f1d427
Mehedi Hasan <130240814+Mehedi-Hasan-Niloy@users.noreply.github.com>
2026-08-26 10:36:55 +0600

    Add C program to print multiple integer variables
    This program demonstrates how to declare multiple integer variables (num1, num2) and print their values on separate lines in C.

---
+++ multiple_integers.c
@@ -0,0 +1,14 @@
+/* 
+ An integar variable num that will contain 10
+ write a program that prints an integer number
+*/
+#include<stdio.h> 
+int main()
+{
+  int num1=20;
+  int num2=30;
+  printf("Number1 is %d\n",num1);
+  printf("Number2 is %d\n",num2);
+  return 0;
+
+}
