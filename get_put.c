5f9559adc30d7ab170ced49a142cb576101ec4ab
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-07 15:59:02 +0600

    added new c file

---
+++ get_put.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char character;
+
+    printf("Enter any character : ");
+    character = getchar();
+    printf("You entered : ");
+    putchar(character);
+}
---
+++ getchar.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char character;
+
+    printf("Enter any character : ");
+    character = getchar();
+    printf("You entered : %c\n",character);
+}
