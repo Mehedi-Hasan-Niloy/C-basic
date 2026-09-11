8975e7f5a3d4147629cee72cc83675612b4b285b
Mehedi Hasan <mehedihasan1231237@gmail.com>
2026-09-11 17:10:41 +0600

    new

---
+++ average.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int num1, num2, sum;
+    float avg;
+    printf("Enter two number : ");
+    scanf("%d %d",&num1,&num2);
+
+    sum = num1 + num2;
+    printf("The sum is = %d\n",sum);
+
+    avg = (float)sum/2; //type casting
+    printf("The average is = %.2f\n",avg);
+
+    return 0;
+}
